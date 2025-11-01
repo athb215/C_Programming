85d4d0768356434e2c4e8c6f5f4ecd238f390f45
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:14:28 +0530

    Update

--- Assignments/Assignment_11/program11_3.c
+++ Assignments/Assignment_11/program11_3.c
@@ -6,7 +6,7 @@
 //  Description :   To display sum of all numbers between range
 //  Input :         int, int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////

9777eb3ff864fc9bedac425dbdba53a2925a096b
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 18:58:56 +0530

    Update author name in program2_5.c

--- Assignments/Assignment_02/program2_5.c
+++ Assignments/Assignment_02/program2_5.c
@@ -7,7 +7,7 @@
 //  Description :   Check whether input is even or odd
 //  Input :         int
 //  Output :        bool
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////

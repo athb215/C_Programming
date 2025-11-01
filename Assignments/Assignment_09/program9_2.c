b816d3e1a760c201ee7c473232f67ca9d7a38c7a
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:11:50 +0530

    Update

--- Assignments/Assignment_09/program9_2.c
+++ Assignments/Assignment_09/program9_2.c
@@ -6,7 +6,7 @@
 //  Description :   To convert value of USD in rupees 
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////

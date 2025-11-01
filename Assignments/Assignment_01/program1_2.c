87ba36697e6cd3487c7f551d166c41a37c50a2ed
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 18:55:13 +0530

    update

--- Assignments/Assignment_01/program1_2.c
+++ Assignments/Assignment_01/program1_2.c
@@ -6,7 +6,7 @@
 //  Description :   It is used to Display "Marvellous" 5 times
 //  Input :         void
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////

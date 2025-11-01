a3f929ef00ac56a0e8649a0a825e1f58d215adb1
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 18:57:45 +0530

    Update author name in program2_3.c

--- Assignments/Assignment_02/program2_3.c
+++ Assignments/Assignment_02/program2_3.c
@@ -6,7 +6,7 @@
 //  Description :   Reply Hello and Demo as per user input
 //  Input :         int
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////

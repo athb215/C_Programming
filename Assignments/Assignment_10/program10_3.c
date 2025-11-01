087e467476658f256cbdd61112903026f9362c20
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:13:21 +0530

    Update

--- Assignments/Assignment_10/program10_3.c
+++ Assignments/Assignment_10/program10_3.c
@@ -6,7 +6,7 @@
 //  Description :   To convert distance kilometer into meter
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////

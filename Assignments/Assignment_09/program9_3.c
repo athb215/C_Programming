dde7f081045eb43be86a61d93c518d131d0cbeee
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:12:04 +0530

    Update

--- Assignments/Assignment_09/program9_3.c
+++ Assignments/Assignment_09/program9_3.c
@@ -6,7 +6,7 @@
 //  Description :   To get event facotrial of input number
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////

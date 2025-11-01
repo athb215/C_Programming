298023a829d7d118081ac9a06bd30b72f0699956
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:18:07 +0530

    Update

--- Assignments/Assignment_14/program14_4.c
+++ Assignments/Assignment_14/program14_4.c
@@ -6,7 +6,7 @@
 //  Description :   To check frequency of 4 in number
 //  Input :         int
 //  Output :        void
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Atharva Kaustubh Bhutkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
